f7d0ca777b5b4a4e675e27328a3c43476cd45044
ShaileshParigi <shaileshparigi13@gmail.com>
2023-07-19 20:59:03 +0530

    Updated Mobile

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -4,7 +4,10 @@ import { technologies } from "../constants";
 
 const Tech = () => {
   return (
-    <div className="flex flex-row flex-wrap justify-center gap-10">
+    <div
+      className="flex flex-row flex-wrap justify-center gap-10"
+      style={{ flexWrap: "wrap" }}
+    >
       {technologies.map((technology) => (
         <div className="w-28 h-28" key={technology.name}>
           <BallCanvas icon={technology.icon} />
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -22,8 +22,8 @@ const Computers = ({ isMobile }) => {
       <primitive
         object={computer.scene}
         scale={isMobile ? 0.6 : 0.75}
-        position={isMobile ? [0, -3, -2.2] : [0, -3.75, -1.5]}
-        rotation={[-0.01, 0.01, -0.1]}
+        position={isMobile ? [0, -3.25, -2.0] : [0, -3.75, -1.5]}
+        rotation={[-0.01, 0, -0.1]}
         // rotation={[0, Math.PI * 2, 0]}
       />
     </mesh>
