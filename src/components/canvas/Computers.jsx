99880658bfd184247477a8e3551c2402943588b4
ShaileshParigi <shaileshparigi13@gmail.com>
2023-07-19 18:37:48 +0530

    updated Hero

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -21,9 +21,9 @@ const Computers = ({ isMobile }) => {
       <pointLight intensity={1} />
       <primitive
         object={computer.scene}
-        scale={isMobile ? 0.7 : 0.75}
-        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
-        rotation={[-0.01, 0.35, -0.1]}
+        scale={isMobile ? 0.6 : 0.75}
+        position={isMobile ? [0, -3, -2.2] : [0, -3.75, -1.5]}
+        rotation={[-0.01, 0.01, -0.1]}
         // rotation={[0, Math.PI * 2, 0]}
       />
     </mesh>
@@ -59,7 +59,7 @@ const ComputersCanvas = () => {
       frameloop="demand"
       shadows
       dpr={[1, 2]}
-      camera={{ position: [20, 3, 5], fov: 25 }}
+      camera={{ position: [25, 3, 5], fov: 25 }}
       gl={{ preserveDrawingBuffer: true }}
     >
       <Suspense fallback={<CanvasLoader />}>
